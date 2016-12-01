eeb5ee444f8f3292036b857e7782f446646f6715
ping55yo <pongyo55@gmail.com>
2016-12-02 01:50:09 +0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,12 +1,21 @@
 from tkinter import *
+from tkinter import messagebox
 def sv():
     #s=str(input("Server: "))
     print("OK")
+#def discon(event):
+    messagebox.showinfo(title='แสดงข้อคววาม',message="ตัดการเชื่อมต่อแล้ว")
+#def con(evnet):
+    messagebox.showinfo(title='แสดงข้อคววาม', message="เชื่อมต่อแล้ว")
+
 gui=Tk()
 gui.geometry("550x550")
 gui.title("TF:Talk friend")
 mlabel=Label(text="Talk to friends",fg="blue").pack()
 mButton=Button(text="connect",command=sv).pack()
+#mButton.bind(Button>-1,con).pack()
+disconnect=Button(text="disconnect",command=sv).pack()
+#disconnect.bind('Button-1',discon).pack()
 #objEntry=Entry().pack()
 menubar=Menu(gui)
 #เมนูการเชื่อมต่อ
